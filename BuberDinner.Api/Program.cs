2b922651204acee33de93700c9e034239039af18
harunaabaldeh <harunaabaldeh@gmail.com>
2024-02-19 13:08:23 +0000

    added global error handling

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,15 +1,24 @@
+using BuberDinner.Api.Errors;
+using BuberDinner.Api.Filters;
+using BuberDinner.Api.Middleware;
 using BuberDinner.Application;
 using BuberDinner.Infrastracture;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services.AddApplication();
     builder.Services.AddInfrastracture(builder.Configuration);
+    // builder.Services.AddControllers(options => { options.Filters.Add<ErrorHandlingFilterAttribute>(); });
     builder.Services.AddControllers();
+
+    builder.Services.AddSingleton<ProblemDetailsFactory, BuberDinnerProblemsDetailFactory>();
 }
 
 var app = builder.Build();
 {
+    // app.UseMiddleware<ErrorHandlingMiddleware>();
+    app.UseExceptionHandler("/error");
     app.UseHttpsRedirection();
     app.MapControllers();
     app.Run();
--- BuberDinner.Application/Common/Interfaces/Persistence/IUserRepository.cs
+++ BuberDinner.Application/Common/Interfaces/Persistence/IUserRepository.cs
@@ -5,5 +5,4 @@ public interface IUserRepository
 {
     User? GetByEmail(string email);
     void Add(User user);
-
 }
