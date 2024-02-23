4805bb3840a6491ad8c22eb78174041e080ae1a9
harunaabaldeh <harunaabaldeh@gmail.com>
2024-02-23 09:06:18 +0000

    Added mediatr and fluent validation

--- BuberDinner.Application/DependancyInjection.cs
+++ BuberDinner.Application/DependancyInjection.cs
@@ -1,3 +1,9 @@
+using System.Reflection;
+using BuberDinner.Application.Authentication.Commands.Register;
+using BuberDinner.Application.Common.Behaviors;
+using BuberDinner.Application.Services.Authentication.Common;
+using ErrorOr;
+using FluentValidation;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -7,6 +13,10 @@ public static class DependancyInjection
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddMediatR(typeof(DependancyInjection).Assembly);
+
+        services.AddScoped(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
         return services;
     }
 }
